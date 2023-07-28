b157e85757d7459dc69e8d3b4b39bf964ee24ac0
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-07-28 23:06:24 +0300

    28.07.23_1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const myFunctions = require("./task");
 
-<<<<<<< HEAD
 const {
   functionExample,
   isValidWalk,
@@ -33,77 +32,76 @@ console.log(
 );
 
 function nextSmaller(n) {
-    const arr = String(n).split(""); //n.toString() 28706004446
-    // let d = 2;
-    // let number = arr[arr.length - d] + arr[arr.length - 1];
-    // console.log(+number);
-  
-    // while () {
-    //   d++;
-    //   number = arr[arr.length - d] + number;
-    //   console.log(d);
-    //   nextSmaller(+number);
-    // }
-  
-    for (let i = arr.length - 1; i >= 0; i--) {
-      for (let j = i - 1; j >= 0; j--) {
-        if (arr[i] < arr[j]) {
-          arr.splice(j, 0, ...arr.splice(i, 1));
-          // console.log(arr);
-          const l = arr.splice(j + 1, arr.length - 1).sort((a, b) => b - a);
-          return arr.concat(l).join("").startsWith("0")
-            ? -1
-            : +arr.concat(l).join("");
-        }
-        // if (arr[i] === arr[j]) {
-        //   if (i === j + 1) break;
-        //   arr.splice(j + 1, 0, ...arr.splice(j, 1));
-        //   const l = arr.splice(j + 2, arr.length - 1).sort((a, b) => b - a);
-        //   return arr.concat(l).join("")
-        //     ? -1
-        //     : +arr.concat(l).join("") === n
-        //     ? -1
-        //     : +arr.concat(l).join("");
-        // }
+  const arr = String(n).split(""); //n.toString() 28706004446
+  // let d = 2;
+  // let number = arr[arr.length - d] + arr[arr.length - 1];
+  // console.log(+number);
+
+  // while () {
+  //   d++;
+  //   number = arr[arr.length - d] + number;
+  //   console.log(d);
+  //   nextSmaller(+number);
+  // }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[i] < arr[j]) {
+        arr.splice(j, 0, ...arr.splice(i, 1));
+        // console.log(arr);
+        const l = arr.splice(j + 1, arr.length - 1).sort((a, b) => b - a);
+        return arr.concat(l).join("").startsWith("0")
+          ? -1
+          : +arr.concat(l).join("");
       }
+      // if (arr[i] === arr[j]) {
+      //   if (i === j + 1) break;
+      //   arr.splice(j + 1, 0, ...arr.splice(j, 1));
+      //   const l = arr.splice(j + 2, arr.length - 1).sort((a, b) => b - a);
+      //   return arr.concat(l).join("")
+      //     ? -1
+      //     : +arr.concat(l).join("") === n
+      //     ? -1
+      //     : +arr.concat(l).join("");
+      // }
     }
-    return -1;
   }
-  
-  console.log("nextSmaller  :>> ", nextSmaller(28706006446)); //22855577
+  return -1;
+}
+
+console.log("nextSmaller  :>> ", nextSmaller(28706006446)); //22855577
 //   2829214;
-  
+
 //   2829142;
-  
+
 //   28706004664;
-  
+
 //   28706006446;
 //   28706004664;
-  
+
 //   28706003446
 //   28704664300
-  
+
 //   28704664400
 //   28700664440
-  
-  
+
 //   28706004654;
 //   28706004654;
-  
+
 //   28706005446;
-  
+
 //   28706004446;
-  
+
 //   28704664400;
-  
+
 //   28676544000;
 //   28700665440;
-  
+
 //   1274092203;
 //   1274092032;
-  
+
 //   1274092023;
 //   1274039220;
-  
+
 //   1234567908;
 //   1234567;
